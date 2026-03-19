b224cfc81576f914ffe27f90e1067a5601eccc9b
existshtr <86368110+existshtr@users.noreply.github.com>
2026-03-19 14:42:31 +0700

    Add files via upload

---
+++ js3.25/main.js
@@ -0,0 +1,15 @@
+/* for (i = 1; i <= 20; i++)
+  if (i % 4 !== 0) {
+    console.log(i);
+  }
+*/
+
+/*
+const number = +prompt("Введите число");
+let factorial = 1;
+for ( let i = 1; i <= number; i++){
+  factorial *= i;
+}
+console.log(factorial);
+*/
+
